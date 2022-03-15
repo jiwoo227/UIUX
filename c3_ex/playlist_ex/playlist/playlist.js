d3cbde3625aa53c231c4d016eed88d92f92e27a1
jiwoo227 <w2040@e-mirim.hs.kr>
2022-03-15 09:31:50 +0900

    3.15 1교시

--- c3_ex/playlist_ex/playlist/playlist.js
+++ c3_ex/playlist_ex/playlist/playlist.js
@@ -9,6 +9,22 @@ function init() {
 	loadPlaylist();
 }
 
+function handleButtonClick(e){
+	var textInput = document.getElementById("songTextInput");
+	var songName = textInput.ariaValueMax;
+	//alert("Adding" + songName)
+
+	if(songName == ""){
+		alert("곡을 입력하세요");
+	}else{
+		var li= document.createElement("li");
+		li.innerHTML = songName;
+		var ul = document.getElementById("playlist");
+		ul.appendChild(li);
+		save(songName);
+	}
+}
+
 	//추가1
 
 
